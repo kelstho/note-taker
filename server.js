edbf4495b3673247e765001736461b81900bd715
Kelly Thompson <ksthompson48@gmail.com>
2020-04-09 15:04:31 -0500

    restructure server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ let app = express();
 
 let PORT = process.env.PORT || 8080;
 
-//require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
