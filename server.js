14988baedbb0933805822a1e2e4b242bf20da414
Kelly Thompson <ksthompson48@gmail.com>
2020-04-09 15:30:03 -0500

    add static public

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ let PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
-require("./routes/apiRoutes")(app);
+//require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
