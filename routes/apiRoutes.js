eedd4d613dfae5255286965cd2b157d753dd841b
Kelly Thompson <ksthompson48@gmail.com>
2020-04-10 17:20:46 -0500

    modify spacing for easier readability

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-
 module.exports = (app) => {
     const filePath = "../db/db.json";
+    
     app.get("/api/notes", (req, res) => {
         res.sendFile(path.join(__dirname, filePath));
     });
@@ -14,9 +14,7 @@ module.exports = (app) => {
             text: req.body.text,
             id: Date.now()
         };
-
         const db = JSON.parse(fs.readFileSync(path.join(__dirname, filePath), "utf8"));
-
         db.push(newNote);
         fs.writeFileSync(path.join(__dirname, filePath), JSON.stringify(db), "utf8");
         console.log(db);
