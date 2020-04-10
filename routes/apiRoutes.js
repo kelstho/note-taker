8055f11bf18d53fd37a5b428a0bc5ce4dce36fdc
Kelly Thompson <ksthompson48@gmail.com>
2020-04-10 16:12:11 -0500

    adding notes to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,31 @@
 const fs = require("fs");
-const database = "db/db.json";
+const path = require("path");
 
-let db = JSON.parse(fs.readFileSync(database, 'utf8', function (err) {
+const db = JSON.parse(fs.readFileSync("db/db.json", "utf8", (err) => {
     if (err) {
         console.log(err);
     }
 }));
 
-module.exports = function (app) {
+module.exports = (app) => {
 
-    app.get("/api/notes", function (req, res) {
+    app.get("/api/notes", (req, res) => {
         res.json(db);
     });
 
+    app.post("/api/notes", (req, res) => {
+        const read = fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8");
+
+        const note = {
+            title: req.body.title,
+            text: req.body.text,
+            id: Date.now()
+        };
+        console.log(note);
+        const p = JSON.parse(read);
+
+        p.push(note);
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(p), "utf8");
+        res.json(db);
+    });
 };
