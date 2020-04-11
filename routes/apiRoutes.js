9907a9e4ef1353f62ae32de6b6c29d015c95f687
Kelly Thompson <ksthompson48@gmail.com>
2020-04-11 18:12:20 -0500

    delete route functioning

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+const filePath = "../db/db.json";
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, filePath), "utf8"));
 
 module.exports = (app) => {
-    const filePath = "../db/db.json";
-    
+
     app.get("/api/notes", (req, res) => {
         res.sendFile(path.join(__dirname, filePath));
     });
@@ -14,10 +15,15 @@ module.exports = (app) => {
             text: req.body.text,
             id: Date.now()
         };
-        const db = JSON.parse(fs.readFileSync(path.join(__dirname, filePath), "utf8"));
         db.push(newNote);
         fs.writeFileSync(path.join(__dirname, filePath), JSON.stringify(db), "utf8");
-        console.log(db);
+        res.sendFile(path.join(__dirname, filePath));
+    });
+
+    app.delete("/api/notes:id", (req, res) => {
+        let identifier = req.params.id;
+        db.splice(db.findIndex(x => x.id == identifier), 1);
+        fs.writeFileSync(path.join(__dirname, filePath), JSON.stringify(db), "utf8");
         res.sendFile(path.join(__dirname, filePath));
     });
 };
